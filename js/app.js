1d5bd16292461283ffefa83cf541e80828239792
berchior404 <pedro_berchior@outlook.com>
2022-02-10 22:21:52 -0300

    commit lesson 4

--- js/app.js
+++ js/app.js
@@ -9,6 +9,31 @@ class Despesa {
     }
 }
 
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+    getProxId(){
+        let proxId = localStorage.getItem('id')
+        return parseInt(proxId) + 1
+    }
+
+    gravar(d) {
+        let id = this.getProxId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
@@ -25,5 +50,5 @@ function cadastrarDespesa() {
         valor.value
     )
 
-    console.log(despesa)
+    bd.gravar(despesa)
 }
